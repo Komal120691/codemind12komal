836f1fe3c16e7a1723ba619697549f382eed079a
Komal120691 <komalbawane09@gmail.com>
2024-08-02 16:16:45 +0530

    my 2nd commit

---
+++ src/test/java/com/codemind/StringCreating.java
@@ -0,0 +1,23 @@
+package com.codemind;
+
+public class StringCreating {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//creating string by using string literal
+		String s="komal bawane";
+		String s1="1234qwert@#$$4";
+		
+		System.out.println(s);
+
+		
+		//creating string by using new key world
+		String s2=new String ("urvi");
+		char x[]={'a','b','c','d'};
+		String s3 = new String(x);
+		
+		System.out.println(s3);
+
+}
+}
